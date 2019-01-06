813ad7408ec3b98cf7db2577151ae1f8d19917c5
Stephen Wright <scw248@gmail.com>
2019-01-06 21:35:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -6,6 +6,7 @@ def dictionary
       "two" => "2",
       "too" => "2",
       "for" => "4",
+      "For" => "4"
       "four" => "4",
       'be' => 'b',
       'you' => 'u',
@@ -19,12 +20,11 @@ end
 def word_substituter(tweet_one, dictionary)
 
  tweet_array = tweet.split(" ")
-  dictionary
-  dictionary_keys = shortened_dictionary.keys 
+  dictionary_keys = dictionary.keys 
   
  shortened_array = tweet_array.collect do |word|
     if dictionary_keys.include?(word)
-      word = shortened_dictionary[word]
+      word = dictionary[word]
     end
   end
   shortened_array.join(" ")
